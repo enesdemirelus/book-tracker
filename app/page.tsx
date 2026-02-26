5012dad9af857a26e38242dd033d293b197a0b61
Enes Demirel <enesdemirelus@gmail.com>
2026-02-26 15:06:52 -0600

    added dark mode support

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "./components/ModeToggle";
 
 export default function Home() {
   return (
     <>
-      <Button>Click me!</Button>
+      <div className="m-5 flex gap-2">
+        <Button>Click me!</Button>
+        <ModeToggle></ModeToggle>
+      </div>
     </>
   );
 }
